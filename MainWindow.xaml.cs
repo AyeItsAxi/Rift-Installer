38f26cab36cfce627f645704ed7bc07db4caa505
axi <76423997+AyeItsAxi@users.noreply.github.com>
2022-05-07 14:45:45 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Input;
+using System.IO.Compression;
+
+namespace RiftInstaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (Directory.Exists("Rift-2.2.0.5-B"))
+            {
+                InstallButton.IsEnabled = false;
+                InstallButton.Content = "Installed";
+            }
+        }
+
+        private void DragBar(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Install(object sender, RoutedEventArgs e)
+        {
+            DownloadManager();
+        }
+
+        private void DownloadManager()
+        {
+            try
+            {
+                string gzip = "Rift-2.2.0.5-B.zip";
+                WebClient webclient = new WebClient();
+                webclient.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadRiftCompletedCallback);
+                webclient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(RiftDownloadProgressChanged);
+                webclient.DownloadFileAsync(new Uri("https://cdn.discordapp.com/attachments/972611854524354570/972611918885953546/Rift-2.2.0.5-B.zip"), gzip);
+                InstallButton.Content = "Installing...";
+                InstallButton.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+        public void RiftDownloadProgressChanged(Object sender, DownloadProgressChangedEventArgs e)
+        {
+            Status.Text = "Status: Installing Rift - " + e.ProgressPercentage.ToString() + "%";
+        }
+        private void DownloadRiftCompletedCallback(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string gzip = "Rift-2.2.0.5-B.zip";
+                ZipFile.ExtractToDirectory(gzip, @"./Rift-2.2.0.5-B");
+                File.Delete(gzip);
+                DotNetDownload();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+        private void DotNetDownload()
+        {
+            try
+            {
+                string gzip = "dotnet.exe";
+                WebClient webclient = new WebClient();
+                webclient.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadDotNetCompletedCallback);
+                webclient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DotNetDownloadProgressChanged);
+                webclient.DownloadFileAsync(new Uri("https://download.visualstudio.microsoft.com/download/pr/962fa33f-e57c-4e8a-abc9-01882ff74e3d/23e11ee6c3da863fa1489f951aa7e75e/dotnet-sdk-3.1.417-win-x64.exe"), gzip);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+        public void DotNetDownloadProgressChanged(Object sender, DownloadProgressChangedEventArgs e)
+        {
+            Status.Text = "Status: Downloading .Net - " + e.ProgressPercentage.ToString() + "%";
+        }
+        private void DownloadDotNetCompletedCallback(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string name = "dotnet.exe";
+                Process.Start(name);
+                MessageBox.Show("Because of how installing .Net works, you will have to click ok on this message box when you have finished installing .Net", "Notice", MessageBoxButton.OK, MessageBoxImage.Information);
+                File.Delete(name);
+                MessageBox.Show("Rift has been successfully installed to " + Environment.CurrentDirectory + "\\Rift-2.2.0.5-B", "Download finished!", MessageBoxButton.OK);
+                Status.Text = "";
+                InstallButton.Content = "Install";
+                InstallButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Finishing the download failed! Error message: " + ex.Message);
+            }
+        }
+    }
+}
