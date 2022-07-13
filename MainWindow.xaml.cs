5647c9342713c96f3547de2f8cd0406779635e7b
axi <76423997+AyeItsAxi@users.noreply.github.com>
2022-07-13 13:56:07 -0700

    Environment variable setting (thank you Ahava :))

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Diagnostics;
 using System.Windows.Input;
 using System.IO.Compression;
+using System.Threading.Tasks;
 
 namespace RiftInstaller
 {
@@ -100,16 +101,29 @@ namespace RiftInstaller
                 Process.Start(name);
                 MessageBox.Show("Because of how installing .Net works, you will have to click ok on this message box when you have finished installing .Net", "Notice", MessageBoxButton.OK, MessageBoxImage.Information);
                 File.Delete(name);
+                SetEnvironmentVariable();
                 MessageBox.Show("Rift has been successfully installed to " + Environment.CurrentDirectory + "\\Rift-2.2.0.5-B", "Download finished!", MessageBoxButton.OK);
                 Status.Text = "";
-                InstallButton.Content = "Install";
-                InstallButton.IsEnabled = true;
+                InstallButton.Content = "Installed";
+                InstallButton.IsEnabled = false;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Finishing the download failed! Error message: " + ex.Message);
                 this.Close();
             }
+        }
+
+        private void SetEnvironmentVariable()
+        {
+            Status.Text = "Checking if CPU fix is needed";
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("OPENSSL_ia32cap")))
+            {
+                Status.Text = "CPU fix is needed, applying fix";
+                Environment.SetEnvironmentVariable("OPENSSL_ia32cap", "~0x20000000", EnvironmentVariableTarget.Machine);
+                Status.Text = "CPU fix applied";
+            }
+            Status.Text = "CPU fix is not needed, finishing up";
         }
     }
 }
