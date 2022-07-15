55721ec2a93622766363048114dc93f9bd8f618a
axi <76423997+AyeItsAxi@users.noreply.github.com>
2022-07-14 22:30:00 -0700

    add exclusion to defender, create a desktop icon, save dotnet to appdata, save rift to just a folder called rift, recolor main window elements, change fonts, change window name

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,10 @@ using System.Diagnostics;
 using System.Windows.Input;
 using System.IO.Compression;
 using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
 
 namespace RiftInstaller
 {
@@ -17,7 +21,8 @@ namespace RiftInstaller
         public MainWindow()
         {
             InitializeComponent();
-            if (Directory.Exists("Rift-2.2.0.5-B"))
+            Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Rift Installer\\Data\\");
+            if (Directory.Exists("Rift"))
             {
                 InstallButton.IsEnabled = false;
                 InstallButton.Content = "Installed";
@@ -43,7 +48,16 @@ namespace RiftInstaller
         {
             try
             {
-                string gzip = "Rift-2.2.0.5-B.zip";
+                var start = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    Arguments = "-Command Add-MpPreference -ExclusionPath '" + Environment.CurrentDirectory + "'",
+                    CreateNoWindow = true,
+                };
+                Process.Start(start);
+                string gzip = "Rift.zip";
                 WebClient webclient = new WebClient();
                 webclient.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadRiftCompletedCallback);
                 webclient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(RiftDownloadProgressChanged);
@@ -64,8 +78,8 @@ namespace RiftInstaller
         {
             try
             {
-                string gzip = "Rift-2.2.0.5-B.zip";
-                ZipFile.ExtractToDirectory(gzip, @"./Rift-2.2.0.5-B");
+                string gzip = "Rift.zip";
+                ZipFile.ExtractToDirectory(gzip, @"./Rift");
                 File.Delete(gzip);
                 DotNetDownload();
             }
@@ -82,7 +96,7 @@ namespace RiftInstaller
                 WebClient webclient = new WebClient();
                 webclient.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadDotNetCompletedCallback);
                 webclient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DotNetDownloadProgressChanged);
-                webclient.DownloadFileAsync(new Uri("https://download.visualstudio.microsoft.com/download/pr/962fa33f-e57c-4e8a-abc9-01882ff74e3d/23e11ee6c3da863fa1489f951aa7e75e/dotnet-sdk-3.1.417-win-x64.exe"), gzip);
+                webclient.DownloadFileAsync(new Uri("https://download.visualstudio.microsoft.com/download/pr/962fa33f-e57c-4e8a-abc9-01882ff74e3d/23e11ee6c3da863fa1489f951aa7e75e/dotnet-sdk-3.1.417-win-x64.exe"), Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Rift Installer\\Data\\" + gzip);
             }
             catch (Exception ex)
             {
@@ -98,11 +112,20 @@ namespace RiftInstaller
             try
             {
                 string name = "dotnet.exe";
-                Process.Start(name);
-                MessageBox.Show("Because of how installing .Net works, you will have to click ok on this message box when you have finished installing .Net", "Notice", MessageBoxButton.OK, MessageBoxImage.Information);
-                File.Delete(name);
+                Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Rift Installer\\Data\\" + name);
+                MessageBox.Show("Once .Net is finished installing, run Rift. It may ask if you want to install Microsoft WebView2, if it does, click yes. Try to run Rift again, and if it doesn't, create a ticket in the Rift discord server and the support team will try to assist you as fast as possible.", "Notice", MessageBoxButton.OK, MessageBoxImage.Information);
+                WebClient wc = new WebClient();
+                wc.DownloadFile("https://cdn.discordapp.com/attachments/972611854524354570/997363999995867246/rift.ico", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Rift Installer\\Data\\Rift.ico");
                 SetEnvironmentVariable();
-                MessageBox.Show("Rift has been successfully installed to " + Environment.CurrentDirectory + "\\Rift-2.2.0.5-B", "Download finished!", MessageBoxButton.OK);
+                Status.Text = "Creating desktop icon for Rift";
+                IShellLink link = (IShellLink)new ShellLink();
+                link.SetDescription("Rift Launcher");
+                link.SetPath(Environment.CurrentDirectory + "\\Rift\\Rift.exe");
+                link.SetWorkingDirectory(Environment.CurrentDirectory + "\\Rift");
+                link.SetIconLocation(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Rift Installer\\Data\\Rift.ico", 0);
+                IPersistFile file = (IPersistFile)link;
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                file.Save(Path.Combine(desktopPath, "Rift.lnk"), false);
                 Status.Text = "";
                 InstallButton.Content = "Installed";
                 InstallButton.IsEnabled = false;
@@ -124,6 +147,36 @@ namespace RiftInstaller
                 Status.Text = "CPU fix applied";
             }
             Status.Text = "CPU fix is not needed, finishing up";
+        }
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        internal class ShellLink
+        {
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("000214F9-0000-0000-C000-000000000046")]
+        internal interface IShellLink
+        {
+            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out IntPtr pfd, int fFlags);
+            void GetIDList(out IntPtr ppidl);
+            void SetIDList(IntPtr pidl);
+            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+            void GetHotkey(out short pwHotkey);
+            void SetHotkey(short wHotkey);
+            void GetShowCmd(out int piShowCmd);
+            void SetShowCmd(int iShowCmd);
+            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+            void Resolve(IntPtr hwnd, int fFlags);
+            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
         }
     }
 }
